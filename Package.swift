054984cb1f3870b345ebadbd2938abc11487713d
Pablo Bartolome <pablo.bartolomeherranz@telefonica.com>
2023-05-23 10:17:25 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ let package = Package(
     platforms: [.iOS(.v9)],
     // platforms: [.iOS("9.0"), .macOS("10.10"), tvOS("9.0"), .watchOS("2.0")],
     products: [
-        .library(name: "Lottie", targets: ["Lottie"])
+        .library(name: "Lottie", type: .dynamic, targets: ["Lottie"])
     ],
     targets: [
         .target(
